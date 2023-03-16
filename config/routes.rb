6884adbd56c0a1430a419e945ed49fce9337d206
Italo Lon kan <sanchotorres777@gmail.com>
2023-03-16 14:00:53 -0500

    Add shoppinglists routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   resources :recipes do
     resources :recipe_foods
   end
+  
+  resources :shoppinglists
 
   resources :public_recipes
   post 'toggle_public', to: 'recipes#toggle'
