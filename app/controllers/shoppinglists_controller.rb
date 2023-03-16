a903a98415e155358f23c92d9527913e57017709
Italo Lon kan <sanchotorres777@gmail.com>
2023-03-16 17:59:35 -0500

    Fix rubocop error messages

--- app/controllers/shoppinglists_controller.rb
+++ app/controllers/shoppinglists_controller.rb
@@ -2,39 +2,17 @@ class ShoppinglistsController < ApplicationController
   before_action :find_user
 
   def index
-    @recipe_foods = []
     # get all the recipe_foods from the user's recipes
-    @user.recipes.each do |recipe|
-      recipe.recipe_foods.each do |recipe_food|
-        @recipe_foods << recipe_food
-      end
-    end
+    all_recipes
 
     # sum up the quantities of the same food
-    grouped_recipe_foods = @recipe_foods.group_by(&:food_id)
-    updated_recipe_foods = []
-
-    grouped_recipe_foods.each do |food_id, recipe_foods|
-      quantity_sum = recipe_foods.sum(&:quantity)
-      updated_recipe_foods << RecipeFood.new(food_id:, quantity: quantity_sum,
-                                             recipe_id: recipe_foods.first.recipe_id)
-    end
-
-    @recipe_foods = updated_recipe_foods
+    sum_up_quantities
 
     # subtract the quantities of the foods that are already in the fridge
-    @foods = Food.all
-
-    @recipe_foods.each do |food1|
-      food2 = @foods.find { |f| f.name == food1.food.name }
-      food1.quantity -= food2.quantity if food2
-    end
+    subtract_existing_foods
 
     # get total price
-    @total_price = 0
-    @recipe_foods.each do |recipe_food|
-      @total_price += recipe_food.food.price * recipe_food.quantity
-    end
+    calculate_total_price
   end
 
   private
@@ -46,5 +24,42 @@ class ShoppinglistsController < ApplicationController
   def show
     @recipe = Recipe.includes(:recipe_foods).find(params[:id])
     @recipe_foods = @recipe.recipe_foods
+  end
+
+  def all_recipes
+    @recipe_foods = []
+    @user.recipes.each do |recipe|
+      recipe.recipe_foods.each do |recipe_food|
+        @recipe_foods << recipe_food
+      end
+    end
+  end
+
+  def sum_up_quantities
+    grouped_recipe_foods = @recipe_foods.group_by(&:food_id)
+    updated_recipe_foods = []
+
+    grouped_recipe_foods.each do |food_id, recipe_foods|
+      quantity_sum = recipe_foods.sum(&:quantity)
+      updated_recipe_foods << RecipeFood.new(food_id:, quantity: quantity_sum, recipe_id: recipe_foods.first.recipe_id)
+    end
+
+    @recipe_foods = updated_recipe_foods
+  end
+
+  def subtract_existing_foods
+    @foods = Food.all
+
+    @recipe_foods.each do |food1|
+      food2 = @foods.find { |f| f.name == food1.food.name }
+      food1.quantity -= food2.quantity if food2
+    end
+  end
+
+  def calculate_total_price
+    @total_price = 0
+    @recipe_foods.each do |recipe_food|
+      @total_price += recipe_food.food.price * recipe_food.quantity
+    end
   end
 end
