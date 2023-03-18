6a86974fb3b5b8a2f2084d86ee5f093254256d92
Italo Lon kan <sanchotorres777@gmail.com>
2023-03-17 21:10:05 -0500

    Remove confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :foods, class_name: 'Food', foreign_key: 'user_id'
   has_many :recipes, class_name: 'Recipe', foreign_key: 'user_id'
 
